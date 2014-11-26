ca6bbf23cc7a2bec0f9107b9de2cdd778b2cc585
pietro <p.pilolli@trentinosistemi.com>
2014-11-26 14:24:42 +0100

    Add forgotten javascript file used as bootbox configuration file.

---
+++ l4s/static/js/bootbox.conf.js
@@ -0,0 +1,45 @@
+bootbox.setDefaults({
+  /**
+   * @optional String
+   * @default: it
+   * which locale settings to use to translate the three
+   * standard button labels: OK, CONFIRM, CANCEL
+   */
+  locale: "it",
+  
+  /**
+   * @optional Boolean
+   * @default: true
+   * whether the dialog should be shown immediately
+   */
+  show: true,
+  
+  /**
+   * @optional Boolean
+   * @default: true
+   * whether the dialog should be have a backdrop or not
+   */
+  backdrop: true,
+  
+  /**
+   * @optional Boolean
+   * @default: true
+   * show a close button
+   */
+  closeButton: false,
+  
+  /**
+   * @optional Boolean
+   * @default: true
+   * animate the dialog in and out (not supported in < IE 10)
+   */
+  animate: true,
+  
+  /**
+   * @optional String
+   * @default: null
+   * an additional class to apply to the dialog wrapper
+   */
+  className: "my-modal"
+  
+});
