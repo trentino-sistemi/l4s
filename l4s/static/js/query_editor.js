cac95a4bf5bf8702454b1635ee72d0e7696cf5f1
pietro <p.pilolli@trentinosistemi.com>
2014-12-10 12:53:11 +0100

    - The public queries are shown inside query editor - Better iquery editor search page page layout

---
+++ l4s/static/js/query_editor.js
@@ -0,0 +1,63 @@
+/*
+   This file is part of Lod4stat.
+
+   Copyright (c) 2014 Provincia autonoma di Trento
+
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU Affero General Public License as
+   published by the Free Software Foundation, either version 3 of the
+   License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Affero General Public License for more details.
+
+   You should have received a copy of the GNU Affero General Public License
+   along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*
+  Query editor javascript ajax call.
+*/
+
+function query_editor(table,
+                      columns,
+                      rows,
+                      obs_values,
+                      include_code,
+                      aggregations,
+                      filters,
+                      agg_filters) {
+    spinner = $('#wrap').spin("modal");
+    include_code_value = "false"
+    if (include_code=='True') {
+       include_code_value = "true"
+    }
+
+    url="/query_editor_view/"
+    data = { 'table': table,
+             'columns': columns,
+             'rows': rows,
+             'selected_obs_values': obs_values,
+             'aggregate': aggregations,
+             'filters': filters,
+             'agg_filters': agg_filters,
+             'debug': "False",
+             'include_code': include_code_value,
+              };
+    $.ajax({
+		url: url,
+        type: "POST",
+        data: data,
+        success: function(response) {
+            document.write(response);
+            document.close();
+	    },
+        error: function(xhr, status) {
+            close_spinner(spinner, "modal");
+			bootbox.alert(xhr.responseText);
+		}
+    });
+}
