bebbaf362ca3b7c1367b03ceda87d0b9ad27b43a
Manuel Voltolini <m.voltolini@trentinosistemi.com>
2016-09-27 15:44:18 +0200

    solo superuser puo vedere confini storici/attuali per le tabelle col segreto

--- web/utils.py
+++ web/utils.py
@@ -4624,7 +4624,7 @@ def grouped_by_in_query(user, table_name, column_description):
         value = dict()
 
         if get_key_column_values(column_description[index]['table_name'], column_description[index]['name'], GROUPEDBY) <> []:
-            if user.is_superuser == True or user.is_staff == True or secret == []:  # per super user e membri di staff nessuna limitazione riguardo a secret
+            if user.is_superuser == True or secret == []:  # per super user nessuna limitazione riguardo a secret
                 value['table_name'] = column_description[index]['table_name']
                 value['column_name'] = column_description[index]['name']
                 value['valore'] = '2' #confini attuali
